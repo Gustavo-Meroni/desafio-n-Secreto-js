d21a6282e4d31a703c843a8d50c5de8fbd3c402f
Gustavo Meroni <gustavopmeroni@gmail.com>
2025-05-05 09:42:52 -0300

    primeiro commit

---
+++ app.js
@@ -0,0 +1,32 @@
+alert('Bem vindo ao jogo do número secreto');
+
+let numeroSecreto = 5;
+let chute;
+let tentativas = 1;
+
+while (chute != numeroSecreto) {
+  chute = prompt('Escolha um número entre 1 e 10');
+
+  if (chute == numeroSecreto) {
+    break;
+  } else {
+    if (chute > numeroSecreto) {
+      alert(`O número secreto é menor que ${chute}`);
+    } else {
+      alert(`O número secreto é maior que ${chute}`);
+    }
+    tentativas++;
+  }
+}
+
+if (tentativas > 1) {
+  alert(
+    `Isso ai! você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`,
+  );
+} else {
+  alert(
+    `Isso ai! você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa`,
+  );
+}
+
+console.log(numeroSecreto);
